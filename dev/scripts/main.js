5bcd231e1032b2eaa22ff91f1d5328d1539cc7cd
Brett Nielsen <brettnielsen@rogers.com>
2017-08-30 11:04:22 -0400

    Refactored Yummly and Edamam ajax calls to use consistent formatting/naming

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -4,46 +4,48 @@ const app = {};
 app.idEdamam = 'a4156de2';
 app.keyEdamam = '10efc6df5c7cbcd8288887ca0f20e58c';
 app.urlEdamam = 'https://api.edamam.com/search';
-app.queryEdamam = 'soup';
 
 // ajax call to Edamam
 app.callEdamam = () => {
-	$.ajax({
-		url: `${app.urlEdamam}?q=${app.queryEdamam}&app_id=${app.idEdamam}&app_key=${app.keyEdamam}&health=vegetarian`,
+	var recipeEdamam = $.ajax({
+		url: app.urlEdamam,
+		dataType: 'json'
 		method: 'GET'
+		data: {
+			q: 'soup',
+			app_id: app.idEdamam,
+			app_key: app.keyEdamam
+		}
 	}).then((res) => {
 		console.log(res)
 	});
 }
 
-var yummlyApp = {};
-yummlyApp.yumUrl = 'http://api.yummly.com/v1';
-yummlyApp.yumKey = '2410ab65b1957770177d384fa57c6070';
-yummlyApp.yumId = '95ec33fc';
+// Yummly variables
+app.idYummly = '95ec33fc';
+app.keyYummly = '2410ab65b1957770177d384fa57c6070';
+app.urlYummly = 'http://api.yummly.com/v1';
 
-
-
-yummlyApp.getData = function(){
-	// make an ajax request
-	var receipe = $.ajax({
-	      url : `http://api.yummly.com/v1/api/recipes`,
-	      dataType : 'json',
-	      method: 'GET',
-	      data: {
-	      	  q: 'onion soup',
-	          _app_id: yummlyApp.yumId,
-	          _app_key: yummlyApp.yumKey
-	      }
-
-		}).then(function(res) {
-			console.log(res);
-		})
+// ajax call to Yummly
+app.callYummly = () => {
+	var recipeYummly = $.ajax({
+		url : app.urlYummly,
+		dataType : 'json',
+		method: 'GET',
+		data: {
+			q: 'soup',
+			_app_id: app.idYummly,
+			_app_key: app.keyYummly
+		}
+	}).then((res) => {
+		console.log(res);
+	})
 }
 
 // initialize code
 app.init = () => {
 	app.callEdamam();
-	yummlyApp.getData();
+	app.getData();
 };
 
 // document ready
